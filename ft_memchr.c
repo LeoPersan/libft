7a85d0886dcec3f89edc805b1c1d1770203ba7a9
Leonardo Persan <leopso1990@gmail.com>
2021-07-29 22:06:45 -0300

    fix: casting e biblioteca em memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -10,12 +10,19 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t	i;
+	size_t			i;
+	unsigned char	c_c;
+	unsigned char	*s_c;
 
-	i = 0;
-	while (s[i] && s[i] != c && i < n)
-		i++;
-	return (s + i);
+	s_c = (unsigned char *) s;
+	c_c = (unsigned char) c;
+	i = -1;
+	while (++i < n)
+		if (s_c[i] == c_c)
+			return (s_c + i);
+	return (NULL);
 }
