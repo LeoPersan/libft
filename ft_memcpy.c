0bb0e279033887e3f17807a948e7c23302081562
Leonardo Persan <leopso1990@gmail.com>
2021-07-29 22:01:16 -0300

    fix: casting e biblioteca em memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -10,9 +10,16 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
+	char	*dest_c;
+	char	*src_c;
+
+	dest_c = (char *)dest;
+	src_c = (char *)src;
 	while (n--)
-		dest[n] = src[n];
+		dest_c[n] = src_c[n];
 	return (dest);
 }
