bfcb0885178e9963c377150f6c4600264e3ec760
Leonardo Persan <leopso1990@gmail.com>
2021-07-30 00:07:15 -0300

    fix biblioteca null no caractere size - 1

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -10,16 +10,22 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	size_t	i;
 
 	i = 0;
-	while (src[i] && i < size)
-		dst[i] = src[i++];
-	while (i <= size)
-		dst[i++] = 0;
-	i = 0;
+	if (size--)
+	{
+		while (src[i] && i < size)
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = 0;
+	}
 	while (src[i])
 		i++;
 	return (i);
