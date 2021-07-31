55eeae0ff94e72e7c47400e42add50475f2d7753
Leonardo Persan <leopso1990@gmail.com>
2021-07-31 09:31:02 -0300

    feat: strjoin

---
+++ ft_strjoin.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: leoperei <leopso1990@gmail.com>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/30 21:51:33 by leoperei          #+#    #+#             */
+/*   Updated: 2021/07/30 21:51:33 by leoperei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	i;
+	char	*s3;
+
+	i = 0;
+	s3 = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	while (*s1)
+		s3[i++] = *(s1++);
+	while (*s2)
+		s3[i++] = *(s2++);
+	s3[i] = 0;
+	return (s3);
+}
