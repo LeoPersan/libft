011d30263982745175767c788aae05cd6a411a94
Leonardo Persan <leopso1990@gmail.com>
2021-07-31 21:33:24 -0300

    fix: evitar segmentation fault em strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: leoperei <leopso1990@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/28 20:39:20 by leoperei          #+#    #+#             */
-/*   Updated: 2021/07/28 20:39:20 by leoperei         ###   ########.fr       */
+/*   Updated: 2021/07/31 21:31:49 by leoperei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,11 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
 
-	i = -1;
-	while (++i < n)
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
-	return (0);
+	if (!n)
+		return (0);
+	i = 0;
+	while (i < n && s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	i -= (i == n);
+	return ((unsigned char) s1[i] - (unsigned char) s2[i]);
 }
