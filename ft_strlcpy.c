bd50b0938e064d3c907dcaef5aa7cbcc387e5a60
Leonardo Persan <leopso1990@gmail.com>
2021-08-03 13:03:56 -0300

    refactor: removendo chaves

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: leoperei <leopso1990@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/28 20:37:07 by leoperei          #+#    #+#             */
-/*   Updated: 2021/08/01 15:23:56 by leoperei         ###   ########.fr       */
+/*   Updated: 2021/08/03 12:54:46 by leoperei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,8 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 		return (0);
 	i = -1;
 	while (src[++i])
-	{
 		if (i + 1 < size)
 			*(dst++) = src[i];
-	}
 	if (size > 0)
 		*dst = 0;
 	return (i);
