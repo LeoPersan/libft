74b59b532bfebc7db1fa52c6276a0b9074c27036
Leonardo Persan <leopso1990@gmail.com>
2021-08-03 21:26:39 -0300

    feat: ft_lstclear

---
+++ ft_lstclear.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: leoperei <leopso1990@gmail.com>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/03 20:07:08 by leoperei          #+#    #+#             */
+/*   Updated: 2021/08/03 20:07:08 by leoperei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	if (!(*lst))
+		return ;
+	if ((*lst)->next)
+		ft_lstclear(&(*lst)->next, del);
+	(*del)((*lst)->content);
+	(*del)((*lst));
+	*lst = 0;
+}
