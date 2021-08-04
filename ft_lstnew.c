d210ddaebfc27f0a26ce44fd0ae04d824c992d92
Leonardo Persan <leopso1990@gmail.com>
2021-08-04 20:18:56 -0300

    fix: tratar erro do malloc

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -17,6 +17,8 @@ t_list	*ft_lstnew(void *content)
 	t_list	*lst;
 
 	lst = (t_list *) malloc(sizeof(t_list));
+	if (!lst)
+		return (NULL);
 	lst->content = content;
 	lst->next = NULL;
 	return (lst);
