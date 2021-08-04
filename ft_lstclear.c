9f80ab3369e11bd51624faf37eb6c6535d983027
Leonardo Persan <leopso1990@gmail.com>
2021-08-04 20:19:31 -0300

    fix: desalocar memoria com free

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -19,6 +19,6 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 	if ((*lst)->next)
 		ft_lstclear(&(*lst)->next, del);
 	(*del)((*lst)->content);
-	(*del)((*lst));
+	free(*lst);
 	*lst = 0;
 }
